3d99bd90e0974ce353f059510525ddbf872f9bf3
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2013-12-18 02:27:10 -0400

    Added javadoc on AprioriRunner
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- src/br/edu/ufam/icomp/apriori/AprioriRunner.java
+++ src/br/edu/ufam/icomp/apriori/AprioriRunner.java
@@ -14,64 +14,67 @@ import org.apache.hadoop.util.Tool;
 
 import br.edu.ufam.icomp.MushroomCount;
 
+/**
+ * Classe responsável pela configuração, execução e controle dos K passos
+ * solicitados para a implementação apriori mapreduce
+ */
 public class AprioriRunner extends Configured implements Tool {
 
     public int run(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
 
         if (args.length != 5) {
-            System.err
-                    .println("Parametros: dir-de-entrada dir-de-saida max-de-passos percentual-de-suporte numero-de-registros");
+            System.err.println("Parâmetros: dir-entrada dir-saida passos suporte-minimo total-registros");
             return -1;
         }
 
+        // Armazena todas as variáveis passadas como parâmetros no console
         String entrada = args[0];
         String saida = args[1];
         int maximoPassos = Integer.parseInt(args[2]);
         float suporteMinimo = Float.parseFloat(args[3]);
         int numeroReg = Integer.parseInt(args[4]);
-        long startTime = System.currentTimeMillis();
-        long endTime = System.currentTimeMillis();
+
+        // Cria variáveis para controle do tempo de execução de cada passo
+        long inicio = System.currentTimeMillis();
+        long fim = System.currentTimeMillis();
+        // Para cada passo previsto...
         for (int numPasso = 1; numPasso <= maximoPassos; numPasso++) {
-            endTime = System.currentTimeMillis();
+            fim = System.currentTimeMillis();
+            // ...executa o job do haddop, com seus parâmetros
             boolean jobTerminado = executarJob(entrada, saida, numPasso, suporteMinimo, numeroReg);
-            if (!jobTerminado) {
-                System.err.println("Job falhou. Saindo...");
+            if (!jobTerminado) { // Se houve um problema na execução do passo
+                System.err.println("Job falhou. Execução abortada...");
                 return -1;
             }
-            System.out.println("Passo " + numPasso + ": " + (System.currentTimeMillis() - endTime));
+            System.out.println("Passo " + numPasso + ": " + (System.currentTimeMillis() - fim));
         }
-        endTime = System.currentTimeMillis();
-        System.out.println("Tempo total: " + (endTime - startTime));
+        fim = System.currentTimeMillis();
+        System.out.println("Tempo total: " + (fim - inicio));
         return 0;
     }
 
+    /**
+     * Método responsável por configurar o job do hadoop e dispara sua execução.
+     * 
+     * @return resultado da execução (apenas se foi um sucesso ou não)
+     */
     private static boolean executarJob(String entrada, String saida, int passo, float suporteMinimo, int numeroReg)
             throws IOException, InterruptedException, ClassNotFoundException {
         boolean successful = false;
 
-        JobConf jobConfig = new JobConf();
-        jobConfig.setInt("passo", passo);
-        jobConfig.setFloat("suporteMinimo", suporteMinimo);
-        jobConfig.setInt("numeroRegistros", numeroReg);
+        // Adiciona os parâmetros da execução do job, que posteriormente pode
+        // ser recuperado para ser utilizado tanto no Mao quanto no Reduce
+        JobConf configuracao = new JobConf();
+        configuracao.setInt("passo", passo);
+        configuracao.setFloat("suporteMinimo", suporteMinimo);
+        configuracao.setInt("numeroRegistros", numeroReg);
 
-        FileInputFormat.addInputPath(jobConfig, new Path(entrada));
-        FileOutputFormat.setOutputPath(jobConfig, new Path(saida + passo));
+        FileInputFormat.addInputPath(configuracao, new Path(entrada));
+        FileOutputFormat.setOutputPath(configuracao, new Path(saida + passo));
 
-        // TODO : Should I scan all files in this output directory?
-        /*
-         * The large itemsets of pass (K-1) are used to derive the candidate
-         * itemsets of pass K. So, storing the large itemsets of pass (K-1) in
-         * distributed cache, so that it is accessible to pass K MR job.
-         */
-        /*
-         * if (passo > 1) {
-         * DistributedCache.addCacheFile(URI.create("hdfs://127.0.0.1:54310" +
-         * saida + (passo - 1) + "/part-r-00000"), jobConfig);
-         * System.out.println
-         * ("Adicionado o resultado do passo 1 ao cache distribuido."); }
-         */
-
-        Job job = new Job(jobConfig, "passo_" + passo);
+        Job job = new Job(configuracao, "passo_" + passo);
+        // O problema exige que o primeiro passo tenha um código para Map
+        // diferente das demais execuções
         if (passo == 1) {
             job.setMapperClass(AprioriMapPasso1.class);
         } else {
