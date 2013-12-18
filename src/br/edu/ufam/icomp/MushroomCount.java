e8efc8d59e6c55523ef57a69da1b6c9b2cb55c8b
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2013-12-18 02:12:55 -0400

    Entry-point class javadoc
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- src/br/edu/ufam/icomp/MushroomCount.java
+++ src/br/edu/ufam/icomp/MushroomCount.java
@@ -4,6 +4,10 @@ import org.apache.hadoop.util.ToolRunner;
 
 import br.edu.ufam.icomp.apriori.AprioriRunner;
 
+/**
+ * Ponto de entrada da execução do algoritmo apriori mapreduce. Dispara a classe
+ * responsável pelo gestão da execução dos K passos
+ */
 public class MushroomCount {
 
     public static void main(String[] args) throws Exception {
